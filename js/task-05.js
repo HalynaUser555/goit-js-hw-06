51c9219534dd6767f1fb50df21e47aeb9a7e2d52
Halyna <halyna0193@gmail.com>
2023-06-28 18:14:22 +0300

    fixs task5

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,13 @@ const refs = {
     input: document.querySelector('#name-input'),
     span: document.querySelector('#name-output')
 };
+
+const anonymusText = refs.span.textContent;
 refs.input.addEventListener('input', onInputChange);
 function onInputChange(event) {
-    refs.span.textContent = event.currentTarget.value;
+    if (event.currentTarget.value) {
+        refs.span.textContent = event.currentTarget.value;
+    } else {
+        refs.span.textContent = anonymusText;
+    }
 };

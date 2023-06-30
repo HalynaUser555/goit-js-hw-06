eb35514020218c80770af87b478170184425778e
Halyna <halyna0193@gmail.com>
2023-06-30 11:11:14 +0300

    task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,10 @@
-const refs = {
-    fontSizeInput: document.querySelector('#font-size-control'),
-    textElement: document.querySelector('#text')
-}
 
-
-
-
-
+const fontSizeInput = document.querySelector('#font-size-control');
+const textElement = document.querySelector('#text');
+fontSizeInput.addEventListener('input', function () {
+    const fontSize = fontSizeInput.value + 'px';
+    textElement.style.fontSize = fontSize;
+});
 
 
 
